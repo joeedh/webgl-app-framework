838216c52edf63603ffed2ed1f1d3c3e83c1a9af
Joseph Eagar <joeedh@gmail.com>
2020-12-18 17:54:08 -0800

    Forgot to update this file

--- git_pull.sh
+++ git_pull.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+git submodule init
+git submodule update
+
+git submodule foreach --recursive 'git submodule init';
+git submodule foreach --recursive 'git submodule update';
+
 if [ ! -d "./scripts/renderengine" ]; then
   bash clone_renderengine.sh
 fi
