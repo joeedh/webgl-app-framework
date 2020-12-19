e59d08101d424a0c227cdafa5f08ffbcd2b2c191
Joseph Eagar <joeedh@gmail.com>
2020-12-18 17:53:38 -0800

    Silly me, I already had a script for that

--- git-pull.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git submodule init
-git submodule update
-
-git submodule foreach --recursive 'git submodule init';
-git submodule foreach --recursive 'git submodule update';
-
-git pull;
--- git_pull.bat
+++ git_pull.bat
@@ -1,3 +1,9 @@
+git submodule init
+git submodule update
+
+git submodule foreach --recursive 'git submodule init';
+git submodule foreach --recursive 'git submodule update';
+
 git pull
 cd scripts\renderengine
 git pull
