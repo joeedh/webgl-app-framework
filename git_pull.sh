d31f3494b79802969ea8d06d597b22ddf5324b72
Joseph Eagar <joeedh@gmail.com>
2022-02-14 11:29:58 -0800

    add git_pull.sh

--- git_pull.sh
+++ git_pull.sh
@@ -1,18 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-git submodule init
-git submodule update
+git pull && git submodule foreach --recursive git pull
 
-git submodule foreach --recursive 'git submodule init';
-git submodule foreach --recursive 'git submodule update';
-
-if [ ! -d "./scripts/renderengine" ]; then
-  bash clone_renderengine.sh
-fi
-
-git pull
-cd scripts/renderengine
-git pull
-cd ../shadernodes
-git pull
-cd ../
