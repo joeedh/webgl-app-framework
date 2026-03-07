8cf0b9df571da23a5457ca49ed49e8a8e2637d01
Joe Eagar <joeedh@gmail.com>
2026-03-07 10:15:14 +0000

    update devcontainer

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+# fix vscode server
+cat <<EOF >> ~/.bashrc
+export VSCODE_SERVER=`echo \$BROWSER | sed "s/bin\/helpers\/browser.sh/bin/g"`
+echo Found vscode server at \$VSCODE_SERVER
+echo "export PATH=\$VSCODE_SERVER/remote-cli:\$PATH" >> ~/.bashrc
+EOF
+
 # delete vscode's password, whatever it is
 sudo passwd -d vscode
 
 su vscode -c "pnpm i"
 su vscode -c "bash git_pull.sh"
 su vscode -c "bash git_pull.sh"
-# just to be sure
-su vscode -c "git config --global core.editor \"vim\""
-bash --login
