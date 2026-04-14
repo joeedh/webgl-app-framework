d43c9d05d190dc168cacb943cf4ae5dd908e2836
Joe Eagar <joeedh@gmail.com>
2026-04-14 16:03:02 -0700

    add missing file

---
+++ tools/esbuilder.js
@@ -0,0 +1,58 @@
+import * as esbuild from 'esbuild'
+
+let options = {
+  entryPoints: ['./scripts/entry_point.js'],
+  outdir     : "./build",
+  bundle     : true,
+  target     : "es2022",
+  sourcemap  : 'inline',
+  minify     : false,
+  treeShaking: false,
+  logLevel   : "info",
+  format     : "esm",
+  platform   : "browser",
+  external   : ["fs",
+                "electron",
+                "scripts/util/numeric.js",
+                "numeric",
+                "numeric.js",
+                "scripts/util/numeric",
+                "./scripts/util/numeric.js",
+                "./scripts/util/numeric",
+                "./scripts/extern/Math.js",
+                "./scripts/extern/Math",
+                "./scripts/extern/jszip/*",
+  ],
+  splitting  : true,
+  keepNames  : true,
+  logOverride: {"direct-eval": "silent"},
+};
+
+const handlers = {
+  async help() {
+    console.log("\nUsage: esbuilder --watch,-w --help\n");
+  },
+  async build() {
+    await esbuild.build(options)
+  },
+
+  async watch() {
+    let ctx = await esbuild.context(options);
+    await ctx.watch();
+  }
+};
+
+
+let mode = "build";
+for (let arg of process.argv) {
+  if (arg === "-w" || arg === "--watch") {
+    mode = "watch"
+  }
+
+  if (arg === "-h" || arg === "--help") {
+    mode = "help";
+    break;
+  }
+}
+
+await handlers[mode]()
