8e7e76e7121c52534f04a1a6be05083dbb489aff
Joe Eagar <joeedh@gmail.com>
2026-05-23 14:29:47 -0700

    bleh

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -10,12 +10,12 @@ EOF
 # delete vscode's password, whatever it is
 sudo passwd -d vscode
 
-su vscode -c "git config --global set config.editor vim"
-su vscode -c "pnpm i"
-su vscode -c "bash git_pull.sh"
-su vscode -c "bash git_pull.sh"
-
-su vscode -c "pnpm approve-builds @swc/core@1.15.18, core-js@2.6.12, esbuild@0.19.12,            │
-│   unrs-resolver@1.11.1"
-
-echo "export SERVER_HOST=0.0.0.0" >> ~/.bashrc
+#su vscode -c "git config --global set config.editor vim"
+#su vscode -c "pnpm i"
+#su vscode -c "bash git_pull.sh"
+#su vscode -c "bash git_pull.sh"
+#
+#su vscode -c "pnpm approve-builds @swc/core@1.15.18, core-js@2.6.12, esbuild@0.19.12,            │
+#│   unrs-resolver@1.11.1"
+#
+#echo "export SERVER_HOST=0.0.0.0" >> ~/.bashrc
